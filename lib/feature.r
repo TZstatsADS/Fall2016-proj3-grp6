689f7f5dcc3940e429c9931fa88657e63afc3282
Jaime Gacitua <jgacitua@gmail.com>
2016-11-02 14:08:06 -0400

    frameworks for class

--- lib/feature.r
+++ lib/feature.r
@@ -13,6 +13,7 @@ feature <- function(img_dir, img_name, sift_csv, data_name=null) {
         
         n_files=length(list.files(img_dir))/length(img_name);n_files
         
+        
         nR=10
         nG=10
         nB=10
@@ -21,8 +22,22 @@ feature <- function(img_dir, img_name, sift_csv, data_name=null) {
         
         for(j in 1:length(img_name)){
                 for(i in 1:n_files){
+                        cat(paste0(sprintf("%s%s_%04d.jpg",img_dir,img_name[j],i),"\n"))
                         img=readImage(sprintf("%s%s_%04d.jpg",img_dir,img_name[j],i)) # read image
+                        
+                        
+                        # Resizing images
+                        height <- nrow(img)
+                        width <-  ncol(img) 
+                        
+                        if(width > height){
+                          img <- resize(x = img, h = 200)    
+                        } else{
+                          img <- resize(x = img, w = 200)    
+                        }
+                        
                         mat <- imageData(img)
+                        
                         
                         # Caution: the bins should be consistent across all images!
                         rBin <- seq(0, 1, length.out=nR)
@@ -39,9 +54,12 @@ feature <- function(img_dir, img_name, sift_csv, data_name=null) {
         sift <- t(sift)
         sift <- as.data.frame(sift)
         dat <- cbind(sift, dat)
+        
+        dat <- dat[,which(colSums(dat)!=0)]
+        
         ### output constructed features
         if(!is.null(data_name)){
-                save(dat, file=paste0("./output/feature_", data_name, ".RData"))
+                save(dat, file=paste0("./output/feature_eval", data_name, ".RData"))
         }
         return(dat)
 }
