5a8aebd03815574c3a962f8345266091671e70ef
Chenxi(Celia) Huang <ch3129@columbia.edu>
2016-11-02 00:56:25 -0400

    Update main.R
    new change

--- main.R
+++ main.R
@@ -1,7 +1,6 @@
 #############################################
-### Main execution script    for experiments ###
+### Main execution script for experiments ###
 #############################################
-
 ### Author: Chenxi Huang
 ### Project 3
 ### ADS Fall 2016
@@ -26,7 +25,7 @@ img_test_dir <- "./data/zipcode_test/"
 #label_train <- as.numeric(unlist(label_train) == "9")
 num.chicken=1000
 num.dog=1000
-label_train=c(rep(0,num.chicken),rep(1,num.dog)) #length=2000
+label_train=c(rep(1,num.chicken),rep(0,num.dog)) #length=2000
 
 ### Construct visual feature
 source("./lib/feature.R")
@@ -35,13 +34,20 @@ source("./lib/feature.R")
 #tm_feature_test <- system.time(dat_test <- feature(img_test_dir, "img_zip_test"))
 ########### my change #################
 # feature base
-tm_feature_train_base=system.time(dat_train_base <- feature_base('sift_features.csv'));dim(dat_train)
+# for train feature
+tm_feature_train_base=system.time(dat_train_base <- feature_base('sift_features.csv'));dim(dat_train_base)
 # feature RGB
-tm_feature_train_RGB.dog=system.time(dat_train_RGB <- feature_RGB(img_train_dir, "dog"))
-tm_feature_train_RGB.chic=system.time(dat_train_RGB <- feature(img_train_dir, "chicken"))
-tm_feature_train_RGB=system.time(dat_train_RGB <- feature(img_train_dir, c("chicken','dog")))
-dat_train=cbind(dat_train_base,dat_train_RGB)
-tm_feature_train = tm_feature_train_base+tm_feature_train_RGB
+tm_feature_train_RGB.dog=system.time(dat_train_RGB.dog <- feature_RGB(img_train_dir, "dog"));dim(dat_train_RGB.dog);tm_feature_train_RGB.dog
+tm_feature_train_RGB.chic=system.time(dat_train_RGB.chic <- feature_RGB(img_train_dir, "chicken"));dim(dat_train_RGB.chic);tm_feature_train_RGB.chic
+#tm_feature_train_RGB=system.time(dat_train_RGB <- feature_RGB(img_train_dir, c('chicken','dog')));dim(dat_train_RGB);tm_feature_train_RGB
+dat_train_RGB=rbind(dat_train_RGB.chic,dat_train_RGB.dog)
+dat_train=cbind(dat_train_base,dat_train_RGB);dim(dat_train)
+tm_feature_train = tm_feature_train_base+tm_feature_train_RGB.chic+tm_feature_train_RGB.dog;tm_feature_train
+
+
+# for test feature
+#tm_feature_train_base=system.time(dat_train_base <- feature_base(''));dim(dat_train_base)
+tm_feature_test_RGB <- system.time(dat_test_RGB <- feature_RGB(img_test_dir, "image"))
 
 save(dat_train, file="./output/feature_train.RData")
 save(dat_test, file="./output/feature_test.RData")
@@ -53,19 +59,21 @@ source("./lib/test.R")
 ### Model selection with cross-validation
 # Choosing between different values of interaction depth for GBM
 source("./lib/cross_validation.R")
-depth_values <- 11
+depth_values <- seq(3, 11, 2)
 err_cv <- array(dim=c(length(depth_values), 2))
 K <- 3  # number of CV folds
+# create timer 
+tm_err_cv=rep(0,length(depth_values))
 for(k in 1:length(depth_values)){
   cat("k=", k, "\n")
   err_cv[k,] <- cv.function(dat_train, label_train, depth_values[k], K)
+  tm_err_cv[k]=system.time(err_cv[k,])
 }
 save(err_cv, file="./output/err_cv.RData")
-err_cv # system time of cv
-
+err_cv;tm_err_cv # how long for each cv?
 
 # Visualize CV results
-pdf("./fig/cv_results.pdf", width=7, height=5)
+pdf("./figs/cv_results.pdf", width=7, height=5)
 plot(depth_values, err_cv[,1], xlab="Interaction Depth", ylab="CV Error",
      main="Cross Validation Error", type="n", ylim=c(0, 0.15))
 points(depth_values, err_cv[,1], col="blue", pch=16)
