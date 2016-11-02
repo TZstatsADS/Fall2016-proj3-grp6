928d86ad351b60021ba9211757dbd698fcf89780
Chenxi(Celia) Huang <ch3129@columbia.edu>
2016-11-02 12:07:57 -0400

    Update feature.R
    new change

--- lib/feature.R
+++ lib/feature.R
@@ -2,6 +2,10 @@
 ### Construct visual features for training/testing images ###
 #############################################################
 
+#############################################################
+### Construct visual features for training/testing images ###
+#############################################################
+
 ### Author: Chenxi Huang
 ### Project 3
 ### ADS Fall 2016
@@ -9,6 +13,7 @@
 ### load libraries
 library(EBImage)
 library(data.table) # for fread
+library(grDevices) #for HSV
 
 
 feature <- function(img_dir, img_name, data_name=NULL){
@@ -58,19 +63,19 @@ feature_RGB <- function(img_dir, img_name, data_name=NULL){
   ### Output: an .RData file contains processed features for the images
   
   
-  n_files <- length(list.files(img_dir))
-  nR <- 5
-  nG <- 5
-  nB <- 5
- 
+  n_files=length(list.files(img_dir))/length(img_name);n_files
+  
+  nR=8
+  nG=8
+  nB=8
   ### store vectorized pixel values of images
-  dat <- array(dim=c(n_files, nR*nG*nB)) 
-  for(i in 1:n_files){
-    for(j in length(img_name)){
+  dat <- array(dim=c(length(list.files(img_dir)), nR*nG*nB)) 
+  
+  for(j in 1:length(img_name)){
+    for(i in 1:n_files){
     img=readImage(sprintf("%s%s_%04d.jpg",img_dir,img_name[j],i)) # read image
     mat <- imageData(img)
     
-   
     # Caution: the bins should be consistent across all images!
     rBin <- seq(0, 1, length.out=nR)
     gBin <- seq(0, 1, length.out=nG)
@@ -79,8 +84,51 @@ feature_RGB <- function(img_dir, img_name, data_name=NULL){
                                     factor(findInterval(mat[,,2], gBin), levels=1:nG), 
                                     factor(findInterval(mat[,,3], bBin), levels=1:nB)))
     rgb_feature <- as.numeric(freq_rgb$Freq)/(ncol(mat)*nrow(mat)) # normalization
-    dat[i,]=rgb_feature
-  }}
+    q= i + n_files*(j-1) #index to store vectors
+    dat[q,]=rgb_feature
+    }}
+  
+  ### output constructed features
+  if(!is.null(data_name)){
+    save(dat, file=paste0("./output/feature_", data_name, ".RData"))
+  }
+  return(dat)
+}
+
+################################################################################
+feature_HSV <- function(img_dir, img_name, data_name=NULL){
+  
+  ### Construct process features for training/testing images
+  ### Sample simple feature: Extract raw pixel values of features
+  
+  ### Input: a directory that contains images ready for processing
+  ### Output: an .RData file contains processed features for the images
+  
+  
+  n_files <- length(list.files(img_dir))/length(img_name)
+  nH <- 10
+  nS <- 10
+  nV <- 10
+  ### store vectorized pixel values of images
+  dat <- array(dim=c(length(list.files(img_dir)), nH*nS*nV)) 
+  for(i in 1:n_files){
+    for(j in length(img_name)){
+      img=readImage(sprintf("%s%s_%04d.jpg",img_dir,img_name[j],i)) # read image
+       mat <- imageData(img)
+      
+       mat_rgb <- mat;dim(mat_rgb) <- c(nrow(mat)*ncol(mat), 3)
+       mat_hsv <- rgb2hsv(t(mat_rgb))
+       
+       # Caution: determine the bins using all images! The bins should be consistent across all images. The following code is only used for demonstration on a single image.
+       hBin <- seq(0, 1, length.out=nH)
+       sBin <- seq(0, 1, length.out=nS)
+       vBin <- seq(0, 0.005, length.out=nV) 
+       freq_hsv <- as.data.frame(table(factor(findInterval(mat_hsv[1,], hBin), levels=1:nH), 
+                                       factor(findInterval(mat_hsv[2,], sBin), levels=1:nS), 
+                                       factor(findInterval(mat_hsv[3,], vBin), levels=1:nV)))
+       hsv_feature <- as.numeric(freq_hsv$Freq)/(ncol(mat)*nrow(mat)) # normalization
+      dat[i,]=rgb_feature
+    }}
   
   ### output constructed features
   if(!is.null(data_name)){
