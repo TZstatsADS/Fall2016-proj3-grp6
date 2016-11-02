200820dc173f870e29d252b94d1d34aa5ff0be0d
Jaime Gacitua <jgacitua@gmail.com>
2016-11-02 12:41:05 -0400

    Celia's model

---
+++ lib/feature_RGB2.R
@@ -0,0 +1,138 @@
+#############################################################
+### Construct visual features for training/testing images ###
+#############################################################
+
+#############################################################
+### Construct visual features for training/testing images ###
+#############################################################
+
+### Author: Chenxi Huang
+### Project 3
+### ADS Fall 2016
+
+### load libraries
+library(EBImage)
+library(data.table) # for fread
+library(grDevices) #for HSV
+
+
+feature <- function(img_dir, img_name, data_name=NULL){
+  
+  ### Construct process features for training/testing images
+  ### Sample simple feature: Extract raw pixel values of features
+  
+  ### Input: a directory that contains images ready for processing
+  ### Output: an .RData file contains processed features for the images
+
+  
+  n_files <- length(list.files(img_dir))
+  
+  ### determine img dimensions
+  img0 <-  readImage(paste0(img_dir, img_name, "_", 1, ".jpg"))
+  mat1 <- as.matrix(img0)
+  n_r <- nrow(img0)
+  n_c <- ncol(img0)
+  
+  ### store vectorized pixel values of images
+  dat <- array(dim=c(n_files, n_r*n_c)) 
+  for(i in 1:n_files){
+    img <- readImage(paste0(img_dir, img_name, "_", i, ".jpg"))
+    dat[i,] <- as.vector(img)
+  }
+  
+  ### output constructed features
+  if(!is.null(data_name)){
+    save(dat, file=paste0("./output/feature_", data_name, ".RData"))
+  }
+  return(dat)
+}
+##########################################################################
+# store 'sift_features'in 'output' folder
+feature_base <- function(filename){
+  data <- fread(paste0("./output/", filename)) # dim(data)=5000 by 2000
+  data <- t(data)
+  return(data)
+}
+##########################################################################
+feature_RGB <- function(img_dir, img_name, data_name=NULL){
+  
+  ### Construct process features for training/testing images
+  ### Sample simple feature: Extract raw pixel values of features
+  
+  ### Input: a directory that contains images ready for processing
+  ### Output: an .RData file contains processed features for the images
+  
+  
+  n_files=length(list.files(img_dir))/length(img_name);n_files
+  
+  nR=10
+  nG=10
+  nB=10
+  ### store vectorized pixel values of images
+  dat <- array(dim=c(length(list.files(img_dir)), nR*nG*nB)) 
+  
+  for(j in 1:length(img_name)){
+    for(i in 1:n_files){
+    img=readImage(sprintf("%s%s_%04d.jpg",img_dir,img_name[j],i)) # read image
+    mat <- imageData(img)
+    
+    # Caution: the bins should be consistent across all images!
+    rBin <- seq(0, 1, length.out=nR)
+    gBin <- seq(0, 1, length.out=nG)
+    bBin <- seq(0, 1, length.out=nB)
+    freq_rgb <- as.data.frame(table(factor(findInterval(mat[,,1], rBin), levels=1:nR), 
+                                    factor(findInterval(mat[,,2], gBin), levels=1:nG), 
+                                    factor(findInterval(mat[,,3], bBin), levels=1:nB)))
+    rgb_feature <- as.numeric(freq_rgb$Freq)/(ncol(mat)*nrow(mat)) # normalization
+    q= i + n_files*(j-1) #index to store vectors
+    dat[q,]=rgb_feature
+    }}
+
+  ### output constructed features
+  if(!is.null(data_name)){
+    save(dat, file=paste0("./output/feature_RGB2", data_name, ".RData"))
+  }
+  return(dat)
+}
+
+################################################################################
+feature_HSV <- function(img_dir, img_name, data_name=NULL){
+  
+  ### Construct process features for training/testing images
+  ### Sample simple feature: Extract raw pixel values of features
+  
+  ### Input: a directory that contains images ready for processing
+  ### Output: an .RData file contains processed features for the images
+  
+  
+  n_files <- length(list.files(img_dir))/length(img_name)
+  nH <- 10
+  nS <- 10
+  nV <- 10
+  ### store vectorized pixel values of images
+  dat <- array(dim=c(length(list.files(img_dir)), nH*nS*nV)) 
+  for(i in 1:n_files){
+    for(j in length(img_name)){
+      img=readImage(sprintf("%s%s_%04d.jpg",img_dir,img_name[j],i)) # read image
+       mat <- imageData(img)
+      
+       mat_rgb <- mat;dim(mat_rgb) <- c(nrow(mat)*ncol(mat), 3)
+       mat_hsv <- rgb2hsv(t(mat_rgb))
+       
+       # Caution: determine the bins using all images! The bins should be consistent across all images. The following code is only used for demonstration on a single image.
+       hBin <- seq(0, 1, length.out=nH)
+       sBin <- seq(0, 1, length.out=nS)
+       vBin <- seq(0, 0.005, length.out=nV) 
+       freq_hsv <- as.data.frame(table(factor(findInterval(mat_hsv[1,], hBin), levels=1:nH), 
+                                       factor(findInterval(mat_hsv[2,], sBin), levels=1:nS), 
+                                       factor(findInterval(mat_hsv[3,], vBin), levels=1:nV)))
+       hsv_feature <- as.numeric(freq_hsv$Freq)/(ncol(mat)*nrow(mat)) # normalization
+      dat[i,]=rgb_feature
+    }}
+  
+  ### output constructed features
+  if(!is.null(data_name)){
+    save(dat, file=paste0("./output/feature_", data_name, ".RData"))
+  }
+  return(dat)
+}
---
+++ main_MASTER.R
@@ -0,0 +1,111 @@
+#############################################
+### Main execution script for experiments ###
+#############################################
+### Author: Chenxi Huang
+### Project 3
+### ADS Fall 2016
+
+
+
+# Trees 100
+# CV 5
+# Bins for colors 10, new features = 10*10*10=1000
+# error rate 9%
+# Time to train 17 minutes
+
+
+### Specify directories
+setwd('C:/Users/celia/Desktop/Project 3')
+
+#load libraries
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("EBImage")
+library(EBImage) # not available (for R version 3.3.1)
+library(base)
+library(data.table) # for fread
+
+
+img_train_dir <- "./data/zipcode_train/"
+img_test_dir <- "./data/zipcode_test/"
+
+### Import training images class labels
+########### my change #################
+#label_train <- read.table("./data/zip_train_label.txt", header=F)
+#label_train <- as.numeric(unlist(label_train) == "9")
+num.chicken=1000
+num.dog=1000
+label_train=c(rep(1,num.chicken),rep(0,num.dog)) #length=2000
+
+### Construct visual feature
+source("./lib/feature.R")
+
+#tm_feature_train <- system.time(dat_train <- feature(img_train_dir, "img_zip_train"))
+#tm_feature_test <- system.time(dat_test <- feature(img_test_dir, "img_zip_test"))
+########### my change #################
+# feature base
+# for train feature
+tm_feature_train_base=system.time(dat_train_base <- feature_base('sift_features.csv'));dim(dat_train_base)
+# feature RGB
+tm_feature_train_RGB=system.time(dat_train_RGB <- feature_RGB(img_train_dir, c('chicken','dog')));dim(dat_train_RGB);tm_feature_train_RGB
+#ntm_feature_train_HSV=system.time(dat_train_HSV <- feature_HSV(img_train_dir, c('chicken','dog')));dim(dat_train_HSV);tm_feature_train_HSV
+# get rid of columns which only contain 0
+
+dat_train_RGB=dat_train_RGB[,which(colSums(dat_train_RGB)!=0)]
+dim(dat_train_RGB)
+
+dat_train=cbind(dat_train_base,dat_train_RGB);dim(dat_train)
+tm_feature_train = tm_feature_train_base+tm_feature_train_RGB
+tm_feature_train
+
+
+# for test feature
+#tm_feature_train_base=system.time(dat_train_base <- feature_base(''));dim(dat_train_base)
+# tm_feature_test_RGB <- system.time(dat_test_RGB <- feature_RGB(img_test_dir, "image"))
+
+save(dat_train, file="./output/feature_train.RData")
+save(dat_test, file="./output/feature_test.RData")|
+
+### Train a classification model with training images
+source("./lib/train.R")
+source("./lib/test.R")
+
+### Model selection with cross-validation
+# Choosing between different values of interaction depth for GBM
+source("./lib/cross_validation.R")
+depth_values <- seq(3,11,2)
+err_cv <- array(dim=c(length(depth_values), 2))
+K <- 5  # number of CV folds
+for(k in 1:length(depth_values)){
+  cat("k=", k, "\n")
+  err_cv[k,] <- cv.function(dat_train, label_train, depth_values[k], K)
+}
+save(err_cv, file="./output/err_cv.RData")
+err_cv
+
+# Visualize CV results
+ pdf("./figs/cv_results.pdf", width=7, height=5)
+plot(depth_values, err_cv[,1], xlab="Interaction Depth", ylab="CV Error",
+     main="Cross Validation Error", type="n", ylim=c(0, 0.15))
+points(depth_values, err_cv[,1], col="blue", pch=16)
+lines(depth_values, err_cv[,1], col="blue")
+arrows(depth_values, err_cv[,1]-err_cv[,2],depth_values, err_cv[,1]+err_cv[,2], 
+       length=0.1, angle=90, code=3)
+dev.off()
+
+# Choose the best parameter value
+depth_best <- depth_values[which.min(err_cv[,1])]
+par_best <- list(depth=depth_best);par_best
+
+# train the model with the entire training set
+tm_train <- system.time(fit_train <- train(dat_train, label_train, par_best))
+save(fit_train, file="./output/fit_train.RData")
+
+### Make prediction 
+tm_test <- system.time(pred_test <- test(fit_train, dat_test))
+save(pred_test, file="./output/pred_test.RData")
+
+### Summarize Running Time
+cat("Time for constructing training features=", tm_feature_train[1], "s \n")
+cat("Time for constructing testing features=", tm_feature_test[1], "s \n")
+cat("Time for training model=", tm_train[1], "s \n")
+cat("Time for making prediction=", tm_test[1], "s \n")
