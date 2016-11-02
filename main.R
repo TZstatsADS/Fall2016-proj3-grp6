391ebd1f1a0ed3b05ff81a0cc9f47df2e1c06e41
Chenxi(Celia) Huang <ch3129@columbia.edu>
2016-11-01 20:49:54 -0400

    Update main.R

--- main.R
+++ main.R
@@ -1,29 +1,48 @@
 #############################################
-### Main execution script for experiments ###
+### Main execution script    for experiments ###
 #############################################
 
-### Author: Yuting Ma
+### Author: Chenxi Huang
 ### Project 3
-### ADS Spring 2016
+### ADS Fall 2016
 
 ### Specify directories
-#setwd("./proj3_sample")
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd('C:/Users/celia/Desktop/Project 3')
+
+#load libraries
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("EBImage")
+library(EBImage) # not available (for R version 3.3.1)
+library(base)
+library(data.table) # for fread
+
 
 img_train_dir <- "./data/zipcode_train/"
 img_test_dir <- "./data/zipcode_test/"
 
 ### Import training images class labels
-label_train <- read.table("./data/zip_train_label.txt", header=F)
-label_train <- as.numeric(unlist(label_train) == "9")
+########### my change #################
+#label_train <- read.table("./data/zip_train_label.txt", header=F)
+#label_train <- as.numeric(unlist(label_train) == "9")
+num.chicken=1000
+num.dog=1000
+label_train=c(rep(0,num.chicken),rep(1,num.dog)) #length=2000
 
 ### Construct visual feature
-#source("https://bioconductor.org/biocLite.R")
-#biocLite("EBImage")
 source("./lib/feature.R")
 
-tm_feature_train <- system.time(dat_train <- feature(img_train_dir, "img_zip_train"))
-tm_feature_test <- system.time(dat_test <- feature(img_test_dir, "img_zip_test"))
+#tm_feature_train <- system.time(dat_train <- feature(img_train_dir, "img_zip_train"))
+#tm_feature_test <- system.time(dat_test <- feature(img_test_dir, "img_zip_test"))
+########### my change #################
+# feature base
+tm_feature_train_base=system.time(dat_train_base <- feature_base('sift_features.csv'));dim(dat_train)
+# feature RGB
+tm_feature_train_RGB.dog=system.time(dat_train_RGB <- feature_RGB(img_train_dir, "dog"))
+tm_feature_train_RGB.chic=system.time(dat_train_RGB <- feature(img_train_dir, "chicken"))
+tm_feature_train_RGB=system.time(dat_train_RGB <- feature(img_train_dir, c("chicken','dog")))
+dat_train=cbind(dat_train_base,dat_train_RGB)
+tm_feature_train = tm_feature_train_base+tm_feature_train_RGB
+
 save(dat_train, file="./output/feature_train.RData")
 save(dat_test, file="./output/feature_test.RData")
 
@@ -34,24 +53,26 @@ source("./lib/test.R")
 ### Model selection with cross-validation
 # Choosing between different values of interaction depth for GBM
 source("./lib/cross_validation.R")
-depth_values <- seq(3, 11, 2)
+depth_values <- 11
 err_cv <- array(dim=c(length(depth_values), 2))
-K <- 5  # number of CV folds
+K <- 3  # number of CV folds
 for(k in 1:length(depth_values)){
   cat("k=", k, "\n")
   err_cv[k,] <- cv.function(dat_train, label_train, depth_values[k], K)
 }
 save(err_cv, file="./output/err_cv.RData")
+err_cv # system time of cv
+
 
 # Visualize CV results
-#pdf("./fig/cv_results.pdf", width=7, height=5)
+pdf("./fig/cv_results.pdf", width=7, height=5)
 plot(depth_values, err_cv[,1], xlab="Interaction Depth", ylab="CV Error",
      main="Cross Validation Error", type="n", ylim=c(0, 0.15))
 points(depth_values, err_cv[,1], col="blue", pch=16)
 lines(depth_values, err_cv[,1], col="blue")
 arrows(depth_values, err_cv[,1]-err_cv[,2],depth_values, err_cv[,1]+err_cv[,2], 
-      length=0.1, angle=90, code=3)
-#dev.off()
+       length=0.1, angle=90, code=3)
+dev.off()
 
 # Choose the best parameter value
 depth_best <- depth_values[which.min(err_cv[,1])]
@@ -70,6 +91,4 @@ cat("Time for constructing training features=", tm_feature_train[1], "s \n")
 cat("Time for constructing testing features=", tm_feature_test[1], "s \n")
 cat("Time for training model=", tm_train[1], "s \n")
 cat("Time for making prediction=", tm_test[1], "s \n")
-
-print(pred_test)
 
